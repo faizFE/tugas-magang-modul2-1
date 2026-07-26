8a852787db4fe4d9dfa254e4cc85acfe3cdcedfe
faizFE <fhanafi109@gmail.com>
2026-07-26 11:57:33 +0800

    belajar js

--- latihan.js
+++ latihan.js
@@ -1,10 +1,23 @@
-
+let div = document.querySelector('#div')
 div.style.display = "none";
 
 let button = document.querySelector('#tombol')
+let buttonDisplay = false
 
 button.addEventListener("click", (e) => {
     console.log("diklik")
     e.preventDefault()
-    div.style.display = "block";
+    if(!buttonDisplay) {
+        div.style.display = "block";
+        buttonDisplay = true
+    } else {
+        div.style.display = "none";
+        buttonDisplay = false
+    }
 })
+
+let teks = document.querySelector('#teks')
+teks.innerHTML = '<h1>Hallo</h1>'
+
+let tes = document.querySelectorAll('.tess')
+console.log(tes)
